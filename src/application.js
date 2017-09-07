88e192308adf7656afaeb34cfa212f5995cda10a
Cezary B <czarekburzykowski@gmail.com>
2017-09-07 21:07:00 +0200

    some boilerplate

---
+++ src/application.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Application() {
+    return (
+        <View>
+            <Text>Hello there</Text>
+        </View>    
+    );
+}
