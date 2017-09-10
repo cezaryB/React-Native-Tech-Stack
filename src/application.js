e90cfde88fa86bbdd17036c85e564790649414f7
Cezary B <czarekburzykowski@gmail.com>
2017-09-10 15:48:28 +0200

    next steps

--- src/application.js
+++ src/application.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 export default function Application() {
     return (
         <Provider store={createStore(reducers)}>
             <View>
-                <Text>Hello there</Text>
+                <Header title="Redux App" />
+                <LibraryList />
             </View>
         </Provider>    
     );
---
+++ src/components/LibraryList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+    render() {
+        console.log(this.props);
+        return (
+            <Text>Hello</Text>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        libraries: state.libraries
+    };
+};
+
+export default connect(mapStateToProps)(LibraryList);
